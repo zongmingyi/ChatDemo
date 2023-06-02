cc322ee1412eb321e6eb21c1bfdf467800ca8227
zongm <zongmingyi1999@163.com>
2023-06-02 17:25:55 +0800

    用户基本信息实体

---
+++ master/ChatDemo-parent/API/Rigster/src/main/java/org/zmy/entity/UserInfo.java
@@ -0,0 +1,17 @@
+package org.zmy.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserInfo extends CommonField{
+    private Long account;//账户
+    private String password;//密码
+    private String nickname;//昵称
+    private Integer sex;//性别：为1时表示男性，为0时表示女性
+    private Integer age;//年龄
+}
