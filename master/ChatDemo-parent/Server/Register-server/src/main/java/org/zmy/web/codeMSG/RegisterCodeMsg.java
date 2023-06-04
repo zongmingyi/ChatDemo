c912255b3118390550f19520b5128bef82d056a9
zongm <zongmingyi1999@163.com>
2023-06-04 18:52:18 +0800

    注册功能的状态码类

--- master/ChatDemo-parent/Server/Register-server/src/main/java/org/zmy/web/codeMSG/RegisterCodeMsg.java
+++ master/ChatDemo-parent/Server/Register-server/src/main/java/org/zmy/web/codeMSG/RegisterCodeMsg.java
@@ -5,8 +5,11 @@ import org.zmy.codeMSG.CodeMSG;
 
 public class RegisterCodeMsg extends CodeMSG{
 
-    public RegisterCodeMsg(Integer code,String msg){
+    private RegisterCodeMsg(Integer code,String msg){
         super(code,msg);
     }
     public static final RegisterCodeMsg DUPLICATE_REGISTER = new RegisterCodeMsg(4001,"该账户已被注册");
+    public static final RegisterCodeMsg REGISTER_FAIL = new RegisterCodeMsg(4002 ,"账号注册失败");
+
+    public static final RegisterCodeMsg REGISTER_SUCCESS = new RegisterCodeMsg(4003,"账户注册成功");
 }
