7ac7e37aebb6ac2b7d61b27a4206ad10955205e0
zongm <zongmingyi1999@163.com>
2023-06-04 18:53:59 +0800

    用户信息类

--- master/ChatDemo-parent/API/Rigster/src/main/java/org/zmy/entity/UserInfo.java
+++ master/ChatDemo-parent/API/Rigster/src/main/java/org/zmy/entity/UserInfo.java
@@ -11,6 +11,7 @@ import lombok.Setter;
 public class UserInfo extends CommonField{
     private String account;//账户
     private String password;//密码
+    private String salt;//盐值
     private String nickname;//昵称
     private Integer sex;//性别：为1时表示男性，为0时表示女性
     private Integer age;//年龄
